96283ca6ba1333cdc1309a20d1124e95731c1c17
Krishnath <shadyanuj24@gmail.com>
2020-09-20 20:53:07 +0530

    added redux, store and subscribe to the store

--- movie-app/src/components/App.js
+++ movie-app/src/components/App.js
@@ -4,29 +4,56 @@ import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 import '../../src/index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
+class App extends React.Component {
 
-      <div className="main">
-        <div className="tabs">
-          <div className="tab">Movies</div>
-          <div className="tab">Favourites</div>
+  componentDidMount() {
+    const {store} = this.props;
+    // make api call
+    // dispatch an action
+
+    store.subscribe(()=> {
+      console.log('UPDATED');
+      this.forceUpdate();
+    })
+
+    store.dispatch({
+      type : 'ADD_MOVIES',
+      movies : data
+    })
+
+    console.log(store.getState());
+
+    
+  }
+
+
+  render() {
+
+  console.log(this.props.store);
+
+  const movies = this.props.store.getState();
+
+    return (
+      <div className="App">
+        <Navbar />
+  
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favourites</div>
+          </div>
+  
+          <div className="list">
+              {
+                movies.map((movie,index) => (
+                <MovieCard movie={movie} key={'movies-'+index} />
+                ))
+              }
+          </div>
         </div>
-
-        <div className="list">
-            {
-              data.map(movie => (
-              <MovieCard movie={movie} />
-              ))
-            }
-        </div>
-
-      </div>
-
-    </div>  
-  );
+      </div>  
+    );
+  }
 }
 
 export default App;
--- movie-app/src/index.js
+++ movie-app/src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
+
 import App from './components/App';
+import movies from './reducers/index'
+
+const store = createStore(movies);
+
+// console.log(store, store.getState());
+
+// store.dispatch({
+//   type : 'ADD_MOVIES',
+//   movies : [{name : 'superman'}]
+// })
+
+// console.log(store, store.getState());
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store = {store}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
